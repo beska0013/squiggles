ca0380e47c83763640954f0fafa639c483567695
beska0013 <gugushvili.beso0013@gmail.com>
2022-06-16 14:38:10 +0400

    clear app

--- src/app/components/svgChildComponents/sq-header/sq-header.component.ts
+++
@@ -1,9 +0,0 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
-
-@Component({
-  selector: 'app-sq-header',
-  templateUrl: './sq-header.component.html',
-  styleUrls: ['./sq-header.component.scss'],
-  // changeDetection:ChangeDetectionStrategy.OnPush
-})
-export class SqHeaderComponent{}
